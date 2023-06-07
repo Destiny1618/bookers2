81822d5f3b52d663f60ae2ee365dd26afb64edf5
EC2 Default User <ec2-user@ip-172-31-3-6.ap-northeast-1.compute.internal>
2023-06-07 15:39:35 +0000

    Initial Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  get 'relationships/followings'
+  get 'relationships/followers'
 devise_for :users
 root to: "homes#top"
 resources :books, only: [:index, :show, :edit, :destroy, :create, :update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
-  end
-resources :users, only: [:index, :show, :edit, :update]
+end
+resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+end
 get '/home/about' => "homes#about",as: 'about'
 end
